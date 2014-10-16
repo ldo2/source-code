e403cce4c78599b38dffdf970f63a2ec989ad373
Levchenko Denis <ldo2.msiu@gmail.com>
2014-10-17 03:26:46 +0400

    Конец Времен
    И были Демиурги так могущественны, что могли прервать существование Мира в любой момент, и назвали это Концом Времен.

--- main.rb
+++ main.rb
@@ -1,9 +1,17 @@
 require 'YAML'
 require_relative 'source_code'
 
+end_of_time = false
+trap("INT") do
+  end_of_time = true
+end
+
 world = SourceCode::World.new
-while true
+until end_of_time
   puts world.to_yaml
   world.time_lapse
 end
+
+puts world.to_yaml
+puts "End of time"
 
